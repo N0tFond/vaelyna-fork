587af770dac427907262b2a27dfadf23cafebb56
Selunik <88981713+qrlmza@users.noreply.github.com>
2025-06-02 09:27:41 +0200

    【🌹 】Added non verified role logic

--- handlers/guildUserJoin.js
+++ handlers/guildUserJoin.js
@@ -1,10 +1,24 @@
 const Discord = require('discord.js');
+require('dotenv').config();
 
 const guildUserJoin = (client) => {
 
     client.on('guildMemberAdd', async (member) => {
         try {
             const welcomeChannel = process.env.GENERAL_CHANNEL;
+            const nonVerifiedRole = process.env.NONVERIF_ROLE;
+
+            const role = member.guild.roles.cache.get(nonVerifiedRole);
+            if (!role) {
+                console.error(`⚠️ → Le rôle avec l'ID ${nonVerifiedRole} n'existe pas dans le serveur.`);
+                return;
+            }
+
+            await member.roles.add(role).catch(err => {
+                console.error(`⚠️ → Impossible d'ajouter le rôle ${role.name} à l'utilisateur ${member.user.tag}.`, err);
+                return;
+            });
+            console.log(`✅ → Le rôle ${role.name} a été ajouté à l'utilisateur ${member.user.tag}.`);
             
             const avatarURL = member.user.displayAvatarURL({ format: 'png', size: 128 }) || member.user.defaultAvatarURL;
             const username = member.user.username || 'Utilisateur inconnu';
