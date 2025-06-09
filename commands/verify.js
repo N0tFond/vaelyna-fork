5f914babc33d10b9b9682029c6ee2fff2f8c10f9
qrlmza <qrlmza@icloud.com>
2025-06-09 16:38:08 +0200

    🐍 ⟭ Resolution of non-assignment of unverified role

--- commands/verify.js
+++ commands/verify.js
@@ -91,19 +91,6 @@ module.exports = {
                     await interaction.member.roles.remove(nonVerifiedRoleId);
                     
                     const welcomeChannel = process.env.GENERAL_CHANNEL;
-                    const nonVerifiedRole = process.env.NONVERIF_ROLE;
-
-                    const role = member.guild.roles.cache.get(nonVerifiedRole);
-                    if (!role) {
-                        console.error(`⚠️ → Le rôle avec l'ID ${nonVerifiedRole} n'existe pas dans le serveur.`);
-                        return;
-                    }
-
-                    await member.roles.add(role).catch(err => {
-                        console.error(`⚠️ → Impossible d'ajouter le rôle ${role.name} à l'utilisateur ${member.user.tag}.`, err);
-                    });
-
-                    console.log(`✅ → Le rôle ${role.name} a été ajouté à l'utilisateur ${member.user.tag}.`);
                     
                     const avatarURL = member.user.displayAvatarURL({ format: 'png', size: 128 }) || member.user.defaultAvatarURL;
                     const username = member.user.username || 'Utilisateur inconnu';
--- handlers/guildUserJoin.js
+++ handlers/guildUserJoin.js
@@ -4,7 +4,19 @@ require('dotenv').config();
 const guildUserJoin = (client) => {
 
     client.on('guildMemberAdd', async (member) => {
-        // Script déplacé dans ../commands/verify.js - Eviter l'affichage de membres qui leave directement avant la verif
+        const nonVerifiedRole = process.env.NONVERIF_ROLE;
+
+        const role = member.guild.roles.cache.get(nonVerifiedRole);
+        if (!role) {
+            console.error(`⚠️ → Le rôle avec l'ID ${nonVerifiedRole} n'existe pas dans le serveur.`);
+            return;
+        }
+
+        await member.roles.add(role).catch(err => {
+            console.error(`⚠️ → Impossible d'ajouter le rôle ${role.name} à l'utilisateur ${member.user.tag}.`, err);
+        });
+
+        console.log(`✅ → Le rôle ${role.name} a été ajouté à l'utilisateur ${member.user.tag}.`);
     })
 }
 
